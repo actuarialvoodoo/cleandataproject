7eba4a7a7cfe53cfccac0625189950f08ef3b2f6
actuarialvoodoo <rthomas2003@gmail.com>
2015-05-08 23:43:52 -0400

    Added comments outlining major steps in the analysis

--- run_analysis.R
+++ run_analysis.R
@@ -8,16 +8,30 @@
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 init_dir<-"UCI HAR Dataset"
 
+## DOWNLOAD DATA
 if(!file.exists(init_dir)){
      # Only download data if it is missing    
      DateDownloaded<-Sys.time()
 
-     download.file(fileUrl,destfile="./smartphone.zip",method="curl")
+     cat("Downloading File...")
+     download.file(fileUrl,destfile="./smartphone.zip",method="curl", quiet=TRUE)
+     cat(paste(c(rep(".",20)," DONE.\n"),sep="",collapse=""))
+     
+     cat("Unzipping File...")
      unzip("./smartphone.zip")
      file.remove("smartphone.zip")
+     cat(paste(c(rep(".",22)," DONE.\n"),sep="",collapse=""))
      
      ## Create Log file to record date downloaded.
      f_con<-file("lastdownload.log",open="wt")
      writeLines(paste("Date Downloaded:",DateDownloaded),con=f_con)
      close(f_con)
 }
+
+## IMPORT DATA
+
+## MERGE DATA
+
+## CREATE TIDY DATA SET
+
+## EXPORT TIDY DATA SET
