d8b1087592f5128f5fe9d27cfca7d6470501f982
actuarialvoodoo <rthomas2003@gmail.com>
2015-05-09 17:29:18 -0400

    code to import test data

--- run_analysis.R
+++ run_analysis.R
@@ -37,17 +37,35 @@ process_str<-"Importing Features..."
 cat(process_str)
 fPath<-file.path(init_dir,"features.txt")
 features<-read.table(fPath,sep=" ")
-colnames(features)<-c("ColNames")
+colnames(features)<-c("colnum","colname")
 cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
 
 process_str<-"Importing Activity Labels..."
 cat(process_str)
 fPath<-file.path(init_dir,"activity_labels.txt")
 activities<-read.table(fPath, sep=" ")
-colnames(features)<-c("Activity.Code","Activity.Label")
+colnames(activities)<-c("code","label")
 cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
 
+# Load test data
+process_str<-"Importing Test Data..."
+cat(process_str)
 
+fPath<-file.path(init_dir,"test","X_test.txt")
+test_measures<-read.table(fPath)
+colnames(test_measures)<-features$colname
+
+fPath<-file.path(init_dir,"test","y_test.txt")
+test_activity_codes<-read.table(fPath)
+colnames(test_activity_codes)<-c("activitycode")
+
+fPath<-file.path(init_dir,"test","subject_test.txt")
+test_subjects<-read.table(fPath)
+colnames(test_subjects)<-c("subjectcode")
+
+cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
+
+activity_names<-tolower(activities$label[test_activity_codes$activitycode])
 ## MERGE DATA
 
 ## CREATE TIDY DATA SET
