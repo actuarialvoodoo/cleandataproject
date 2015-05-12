dbaf47e5f9a013ca01b1a19516553de7c4c7c268
actuarialvoodoo <rthomas2003@gmail.com>
2015-05-12 00:11:28 -0400

    wrote dataset.load function to remove duplicate code for loading each data set

--- run_analysis.R
+++ run_analysis.R
@@ -37,29 +37,14 @@ features<-common_info.load("features.txt",c("colnum","colname"))
 activities<-common_info.load("activty_labels.txt",c("code","label"))
 
 # Load test data
-process_str<-"Importing Test Data..."
-cat(process_str)
-
-fPath<-file.path(init_dir,"test","X_test.txt")
-test_measures<-read.table(fPath)
-colnames(test_measures)<-features$colname
-
-fPath<-file.path(init_dir,"test","y_test.txt")
-test_activity_codes<-read.table(fPath)
-colnames(test_activity_codes)<-c("activitycode")
-
-fPath<-file.path(init_dir,"test","subject_test.txt")
-test_subjects<-read.table(fPath)
-colnames(test_subjects)<-c("subjectcode")
-
-cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
-
-activity_names<-tolower(activities$label[test_activity_codes$activitycode])
+data_test<-dataset.load("test")
+data_train<-dataset.load("train")
+#activity_names<-tolower(activities$label[test_activity_codes$activitycode])
 
 # Only use the columns that are means or standard deviations
-use_columns<-grepl("mean\\(\\)|std\\(\\)",colnames(test_measures))
+#use_columns<-grepl("mean\\(\\)|std\\(\\)",colnames(test_measures))
 
-data<-cbind(subject=test_subjects,activity=activity_names,test_measures[use_columns])
+#data<-cbind(subject=test_subjects,activity=activity_names,test_measures[use_columns])
 ## MERGE DATA
 
 ## CREATE TIDY DATA SET
@@ -67,13 +52,34 @@ data<-cbind(subject=test_subjects,activity=activity_names,test_measures[use_colu
 ## EXPORT TIDY DATA SET
 
 common_info.load<-function(filename,colnames){
-     
+     # This function loads supports common information which is application
+     # to both test and train datasets.
      process_str<-paste("Importing",filename,"...",collapse="")
      cat(process_str)
      fPath<-file.path(init_dir,filename)
      info<-read.table(fPath,sep=" ")
      colnames(info)<-colnames
      cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
-     
+     info
 }
 
+dataset.load<-function(folder){
+     # This function loads the data set specified in the folder name
+     # provided as an argument
+     load.file<-function(x,f){
+          fPath<-file.path(init_dir,f,paste(x,"_",f,".txt",sep="",collapse=""))
+          data<-read.table(fPath)
+          colnames(data)<-file.colnames[[x]]
+          data
+     }
+     
+     file.prefix<-c("X","y","subject")
+     file.colnames<-list("X"=features$colname,"y"=c("activitycode"),"subject"=c("subjectcode"))
+     
+     process_str<-paste("Importing",folder,"Data...",collapse="")
+     cat(process_str)
+     data<-lapply(file.prefix,load.file,folder)
+     names(data)<-file.prefix
+     cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
+     data
+}
