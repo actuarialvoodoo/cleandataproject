5c2716a58f507ad0b36e5703876e4815894668a8
actuarialvoodoo <rthomas2003@gmail.com>
2015-05-13 00:16:21 -0400

    Added Comments to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,17 +6,25 @@
 
 ## ASSUMPTIONS
 fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-init_dir<-"UCI HAR Dataset"
+init_dir<-"UCI HAR Dataset" 
 
 ## HELPER FUNCTIONS
 common_info.load<-function(filename,colnames){
-     # This function loads supports common information which is application
+     # This function loads common information which is applicable
      # to both test and train datasets.
+     
+     # BEGIN progress indicator
      process_str<-paste("Importing",filename,"...",collapse="")
      cat(process_str)
+     
+     # The following code block creates a system agnostic file path
+     # loads the space delimited data into info and sets the colnames
+     # equal to the column list provided. 
      fPath<-file.path(init_dir,filename)
      info<-read.table(fPath,sep=" ")
      colnames(info)<-colnames
+     
+     # END progress indicator
      cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
      info
 }
@@ -24,7 +32,10 @@ common_info.load<-function(filename,colnames){
 dataset.load<-function(folder){
      # This function loads the data set specified in the folder name
      # provided as an argument
+     
      load.file<-function(x,f){
+          # function is defined solely for use in the lapply statment below.
+          # it loads the tab delimited data in either the train or test folders.
           fPath<-file.path(init_dir,f,paste(x,"_",f,".txt",sep="",collapse=""))
           data<-read.table(fPath)
           colnames(data)<-file.colnames[[x]]
@@ -36,15 +47,17 @@ dataset.load<-function(folder){
      
      process_str<-paste("Importing",folder,"Data...",collapse="")
      cat(process_str)
+     
      data<-lapply(file.prefix,load.file,folder)
      names(data)<-file.prefix
-     cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
-     #data
      
+     # make the activity labels prettier
      activity_names<-tolower(activities$label[data$y[,1]])
      
      # Only use the columns that are means or standard deviations
      use_columns<-grepl("mean\\(\\)|std\\(\\)",colnames(data$X))
+     
+     cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
      
      cbind(subject=data$subject,activity=activity_names,data$X[,use_columns])
      
@@ -55,15 +68,22 @@ dataset.load<-function(folder){
 if(!file.exists(init_dir)){
      # Only download data if it is missing    
      DateDownloaded<-Sys.time()
+     
+     # Download file
      process_str<-"Downloading File..."
      cat(process_str)
+     
      download.file(fileUrl,destfile="./smartphone.zip",method="curl", quiet=TRUE)
+     
      cat(paste(c(rep(".",50-nchar(process_str))," DONE.\n"),sep="",collapse=""))
      
+     # Unzip and Delete file
      process_str<-"Unzipping File..."
      cat(process_str)
+     
      unzip("./smartphone.zip")
      file.remove("smartphone.zip")
+     
      cat(paste(c(rep(".",50 - nchar(process_str))," DONE.\n"),sep="",collapse=""))
      
      ## Create Log file to record date downloaded.
